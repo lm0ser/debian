28e5610b8c7c74296d356ec6366797996dba2eee
Lucas MOSER <lucas.moser@live.fr>
2019-03-22 11:07:55 +0100

    Update install.sh

--- motd/install.sh
+++ motd/install.sh
@@ -1,5 +1,5 @@
 apt install figlet
 cd /usr/bin/
-wget https://raw.githubusercontent.com/lm0ser/motd/master/custom_motd
+wget https://raw.githubusercontent.com/lm0ser/debian9/master/motd/custom_motd
 chmod +x custom_motd
 echo 'custom_motd' >> /etc/update-motd.d/10-uname
